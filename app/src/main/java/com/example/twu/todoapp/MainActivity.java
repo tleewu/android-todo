58d195c9186e46854153d9c8b075c3a4753f9b12
Theo <tleewu@ucla.edu>
2016-07-16 14:14:31 -0700

    remove seeding data code

--- app/src/main/java/com/example/twu/todoapp/MainActivity.java
+++ app/src/main/java/com/example/twu/todoapp/MainActivity.java
@@ -33,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
         itemsAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-        items.add("First Item");
-        items.add("Second Item");
         setupListViewListener();
     }
 
