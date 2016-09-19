454902b818c87b3872a04039fbd8f6e112cb1955
Theo <twu@flipboard.com>
2016-09-19 01:20:54 -0700

    Revert "Add Toolbar instead of default Android action bar"
    This reverts commit 0866d7efcaa32c4e62312383e6e19507193ad1d9.

--- app/src/main/java/com/example/twu/todoapp/MainActivity.java
+++ app/src/main/java/com/example/twu/todoapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -33,9 +32,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-
         ToDoDatabaseHelper databaseHelper = ToDoDatabaseHelper.getInstance(this);
 
         lvItems = (ListView) findViewById(R.id.lvItems);
